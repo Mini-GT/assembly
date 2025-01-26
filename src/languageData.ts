ff054b2a65e7f4c0ee1ed962c0a37d385c3b7f91
MiniGT <bendanillo539@gmail.com>
2025-01-26 23:22:37 +0800

    created data

---
+++ src/languageData.ts
@@ -0,0 +1,50 @@
+import { nanoid } from "nanoid";
+import { ProgrammingLanguagesType } from "./components/Guesses";
+
+export const programmingData: ProgrammingLanguagesType[] = [
+  {
+    id: nanoid(),
+    name: "Html",
+    isStrikethrough: false
+  },
+  {
+    id: nanoid(),
+    name: "CSS",
+    isStrikethrough: false
+  },
+  {
+    id: nanoid(),
+    name: "JavaScript",
+    isStrikethrough: false
+  },
+  {
+    id: nanoid(),
+    name: "React",
+    isStrikethrough: false
+  },
+  {
+    id: nanoid(),
+    name: "Typescript",
+    isStrikethrough: false
+  },
+  {
+    id: nanoid(),
+    name: "NodeJS",
+    isStrikethrough: false
+  },
+  {
+    id: nanoid(),
+    name: "Python",
+    isStrikethrough: false
+  },
+  {
+    id: nanoid(),
+    name: "Ruby",
+    isStrikethrough: false
+  },
+  {
+    id: nanoid(),
+    name: "Assembly",
+    isStrikethrough: false
+  },
+];
