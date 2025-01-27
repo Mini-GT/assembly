dadb5435cd20c1a5ecf437eeca9d82814d95057f
MiniGT <bendanillo539@gmail.com>
2025-01-28 01:37:31 +0800

    added header classname

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header>
-      <div>
+      <div className="Header_Container">
         <h1>Assembly: Endgame</h1>
         <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
       </div>
