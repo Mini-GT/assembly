60cbfcdc590f932a13c03f858235c0fda049d5c8
MiniGT <bendanillo539@gmail.com>
2025-02-28 01:29:31 +0800

    feat: add win and lose component

---
+++ src/components/Lose.tsx
@@ -0,0 +1,10 @@
+export default function Lose() {
+  return (
+    <>
+      <div className="Result_Container Lose">
+        <h1>Game over!</h1>
+        <p>You lose! Better start learning Assembly 😭</p>
+      </div>
+    </>
+  )
+}
---
+++ src/components/Win.tsx
@@ -0,0 +1,10 @@
+export default function Win() {
+  return (
+    <>
+      <div className="Result_Container Win">
+        <h1>You win!</h1>
+        <p>Well done! 🎉</p>
+      </div>
+    </>
+  )
+}
