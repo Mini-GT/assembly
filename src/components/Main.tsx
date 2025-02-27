e0df577e6f3fff561fbfa82e89e71f3b7f14d9f7
MiniGT <bendanillo539@gmail.com>
2025-02-28 01:28:12 +0800

    feat: display the result iif user has won or lost

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,17 +1,33 @@
-import { LanguageProvider } from "../context/LanguageContext";
+import { useLanguage } from "../context/LanguageContext";
+import { useRandomWord } from "../hooks/useGetRandomWord";
 import Guesses from "./Guesses";
 import Header from "./Header";
 import Keypad from "./Keypad";
+import Win from "./Win";
+import Lose from "./Lose";
+import NewGame from "./NewGame";
 
 export default function Main() {
+  const secretWord = useRandomWord();
+  const { languages } = useLanguage()
+  const isWon = secretWord.every(word => word.isGuessed)
+  const isLost = languages.every(language => language.isWrong)
+  
+  function RenderResult() {
+    if (isWon) {
+      return <Win />;
+    } else if (isLost) {
+      return <Lose />;
+    }
+  }
 
   return (
     <div className="Component_Container">
-      <LanguageProvider>
-        <Header />
-        <Guesses />
-        <Keypad />
-      </LanguageProvider>
+      <Header />
+      <RenderResult /> 
+      <Guesses />
+      <Keypad />
+      <NewGame />
     </div>
   )
 }
