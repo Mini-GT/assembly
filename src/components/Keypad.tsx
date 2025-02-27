2e58a95bb42f51ed612088709673300fe3b14203
MiniGT <bendanillo539@gmail.com>
2025-02-28 01:34:03 +0800

    feat: make the languages change its style to let the user know that the letter it clicked is not matching to the secret word

--- src/components/Keypad.tsx
+++ src/components/Keypad.tsx
@@ -6,6 +6,8 @@ import { handleSelected } from "../Redux/wordSlice";
 import { useRandomWord } from "../hooks/useGetRandomWord";
 import { useState } from "react";
 import { AlphabetObjType } from "../types/alphabet.types";
+import { useLanguage } from "../context/LanguageContext";
+import { getFarewellText } from "../utils/farewellText";
 
 const alphabet = [
   "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", 
@@ -16,6 +18,8 @@ const alphabet = [
 export default function Keypad() {
   const secretWord = useRandomWord();
   const dispatch: AppDispatch = useDispatch();
+  const { languages, setLanguages } = useLanguage()
+  const [wrongGuessIndex, setWrongGuessIndex] = useState(0);
 
   // turn alphabet to Obj
   const alphabetObj: AlphabetObjType[] = alphabet.map(letter => ({
@@ -30,30 +34,43 @@ export default function Keypad() {
   function handleLetterClick(letter: BoxItemType['letter']) {
     dispatch(handleSelected(letter))
 
+    // check if the letter exists in secretWord
+    const isMatch = secretWord.some(wordObj => 
+      wordObj.letter.toLowerCase() === letter.toLowerCase()
+    );
+
+    
     // update alphabet state
     setAlphabetState(prevState => 
       prevState.map(state => {
-        
-        if (state.letter === letter) {
-          // Check if the letter exists in secretWord
-          const isMatch = secretWord.some(wordObj => 
-            wordObj.letter.toLowerCase() === letter.toLowerCase()
-          );
-          
-          if(!isMatch) {
-            console.log('wrong answer')
-          }
-
+        if(state.letter === letter) {
           return { 
             ...state,
             isCorrect: isMatch,
             status: isMatch ? "correct" : "wrong"
           };
         }
-
         return state
       })
     );
+
+    // only update languages if it's not a match
+    if(!isMatch && wrongGuessIndex < languages.length) {
+      setLanguages(prevLanguages => {
+        // create a new array with the object at wrongGuessIndex updated
+        return prevLanguages.map((language, index) => {
+          if (index === wrongGuessIndex) {
+            const farewellText = getFarewellText(language.name)
+            return { ...language, isWrong: true };
+          }
+          return language;
+        });
+      });
+
+       // increment the index for the next wrong guess
+       setWrongGuessIndex(prev => prev + 1);
+    };
+
   }
 
   const letterButton = alphabetState.map(alphabet => {
